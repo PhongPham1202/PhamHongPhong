1440f7be5437a7f07e1ad26af6d26ca5e1984166
HongPhong <phamhongphong1202@gmail.com>
2024-05-31 14:27:59 +0700

    1

---
+++ src/main/java/Tuan3/PhamHongPhong/PhamHongPhongApplication.java
@@ -0,0 +1,13 @@
+package Tuan3.PhamHongPhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamHongPhongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamHongPhongApplication.class, args);
+	}
+
+}
